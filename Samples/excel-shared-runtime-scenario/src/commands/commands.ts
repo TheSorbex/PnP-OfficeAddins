b006c9370a047f189e67120363506a8808f49b0b
Oleksii Yavnyk <o.yavnyk@spd-ukraine.com>
2020-09-17 18:05:11 +0300

    example of inserting value from dialogue

--- Samples/excel-shared-runtime-scenario/src/commands/commands.ts
+++ Samples/excel-shared-runtime-scenario/src/commands/commands.ts
@@ -28,6 +28,55 @@ g.btnclosetaskpane = btnCloseTaskpane;
 g.btnconnectservice = btnConnectService;
 g.btndisconnectservice = btnDisconnectService;
 g.btnsumdata = btnSumData;
+g.openDialogueModal = openDialogueModal;
+
+type Params = {
+  row: number;
+  column: number;
+  range: Excel.Range;
+}
+
+const setNextActiveCell = ({row = 0, column = 0, range}: Params) => {
+  if (row || column) {
+      if (range) {
+          const cell = range.getCell(row, column);
+          cell.select();
+      }
+  }
+};
+
+
+type InsertValueInCell = (
+    value: string | number,
+) => Promise<Excel.RequestContext>;
+
+export const insertValueInCell: InsertValueInCell = (value) => {
+  return Excel.run(async context => {
+      const range = context.workbook.getSelectedRange();
+      range.values = [[value]]
+      setNextActiveCell({row: 1, column: 0, range});
+      return context.sync();
+  });
+};
+
+
+
+export function openDialogueModal(event: Office.AddinCommands.Event) {
+    Office.context.ui.displayDialogAsync('https://localhost:3000', {
+        displayInIframe: true,
+        height: 40,
+        width: 20,
+        promptBeforeOpen: false
+    }, asyncResult => {
+        const dialog = asyncResult.value;
+        dialog.addEventHandler(Office.EventType.DialogMessageReceived, arg => {
+                const message: any = JSON.parse(arg.message);
+                insertValueInCell(String(message.res))
+        });
+    });
+
+    event.completed();
+};
 
 export function btnConnectService(event: Office.AddinCommands.Event) {
   console.log('Connect service button pressed');
